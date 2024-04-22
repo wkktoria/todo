74fd46a56a846b97d75c20f9377a650464471c14
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-04-22 23:13:36 +0200

    Prepare frontend for getting languages from backend

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,7 +1,25 @@
-const BASE_URL = "http://localhost:8080/api/hello";
+const BASE_API_URL = "http://localhost:8080/api";
+const CODE_TO_FLAG = {
+    "en": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/64px-Flag_of_the_United_States.svg.png",
+    "pl": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Flag_of_Poland.svg/64px-Flag_of_Poland.svg.png",
+    "de": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/64px-Flag_of_Germany.svg.png",
+};
 const greetingForm = document.getElementById("greetingForm");
 const greetingFormSubmitButton = document.getElementById("greetingFormSubmitButton");
-const greeting = document.getElementById("greeting");
+const greetingHeading = document.getElementById("greetingHeading");
+const languagesContainer = document.getElementById("languagesContainer");
+
+fetch(`${BASE_API_URL}/languages`)
+    .then((response) => response.json())
+    .then((languages) => {
+        languagesContainer.innerHTML = languages.map(language => `
+        <label class="pure-radio">
+            <input name="language" type="radio" value=${language.id}>
+            <img alt="Flag" src=${CODE_TO_FLAG[language.code]}>
+        </label>
+        `).join("\n");
+    })
+    .catch(error => console.log(error));
 
 greetingFormSubmitButton.addEventListener("click", (event) => {
     event.preventDefault();
@@ -12,9 +30,9 @@ greetingFormSubmitButton.addEventListener("click", (event) => {
         name, language
     };
 
-    fetch(`${BASE_URL}/?${new URLSearchParams(greetingFormObject)}`)
+    fetch(`${BASE_API_URL}/hello?${new URLSearchParams(greetingFormObject)}`)
         .then((response) => response.text())
         .then((responseText) => {
-            greeting.innerText = `${responseText}`;
+            greetingHeading.innerText = `${responseText}`;
         }).catch(error => console.error(error));
 });
