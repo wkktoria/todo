f0767443831e461f934d19c0ab316391d8535858
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-04-22 15:24:12 +0200

    Create hello service

--- src/main/java/io/github/wkktoria/HelloServlet.java
+++ src/main/java/io/github/wkktoria/HelloServlet.java
@@ -1,6 +1,5 @@
 package io.github.wkktoria;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,20 +8,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.Optional;
 
 @WebServlet(name = "Hello", urlPatterns = {"/api/hello/*"})
 public class HelloServlet extends HttpServlet {
     private static final String NAME_PARAM = "name";
-
     private final Logger logger = LoggerFactory.getLogger(HelloServlet.class);
+    private final HelloService service;
+
+    /**
+     * Servlet container needs it.
+     */
+    @SuppressWarnings("unused")
+    public HelloServlet() {
+        this(new HelloService());
+    }
+
+    HelloServlet(HelloService service) {
+        this.service = service;
+    }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         logger.info("Got request with params: {}", req.getParameterMap());
-
-        String name = Optional.ofNullable(req.getParameter(NAME_PARAM)).orElse("world");
-
-        resp.getWriter().write("Hello " + name + "!");
+        resp.getWriter().write(service.prepareGreeting(req.getParameter(NAME_PARAM)));
     }
 }
