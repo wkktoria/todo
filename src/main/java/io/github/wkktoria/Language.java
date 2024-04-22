09ade94a5cb587192ffcb334b41f75154bbb2a2c
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-04-22 15:49:16 +0200

    Create language model

---
+++ src/main/java/io/github/wkktoria/Language.java
@@ -0,0 +1,33 @@
+package io.github.wkktoria;
+
+class Language {
+    private final Long id;
+    private String welcomeMessage;
+    private String code;
+
+    public Language(Long id, String welcomeMessage, String code) {
+        this.id = id;
+        this.welcomeMessage = welcomeMessage;
+        this.code = code;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getWelcomeMessage() {
+        return welcomeMessage;
+    }
+
+    public void setWelcomeMessage(String welcomeMessage) {
+        this.welcomeMessage = welcomeMessage;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
