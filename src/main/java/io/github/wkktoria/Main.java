597aaddf89416b9447d822190ee06e326b4d5758
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-04-22 11:43:06 +0200

    Add logger

--- src/main/java/io/github/wkktoria/Main.java
+++ src/main/java/io/github/wkktoria/Main.java
@@ -1,7 +1,10 @@
 package io.github.wkktoria;
 
+import org.slf4j.LoggerFactory;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var logger = LoggerFactory.getLogger(Main.class);
+        logger.info("Hello World!");
     }
 }
