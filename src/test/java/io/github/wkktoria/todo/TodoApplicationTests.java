cc37ec3bdde77e99306e98e7934fe244feea1461
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-04-24 12:06:41 +0200

    Initialize project

---
+++ src/test/java/io/github/wkktoria/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wkktoria.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
